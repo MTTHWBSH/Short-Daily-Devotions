d3cdbd646a4a13835859db77c28002a314c0e5d8
MTTHWBSH <bushmat4@gmail.com>
2016-08-26 17:17:59 -0500

    pods

---
+++ Pods/Target Support Files/Pods-Short Daily Devotions/Pods-Short Daily Devotions-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Short_Daily_Devotions : NSObject
+@end
+@implementation PodsDummy_Pods_Short_Daily_Devotions
+@end
---
+++ Pods/Target Support Files/Pods-Short Daily Devotions/Pods-Short Daily Devotions-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Short_Daily_DevotionsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Short_Daily_DevotionsVersionString[];
+
