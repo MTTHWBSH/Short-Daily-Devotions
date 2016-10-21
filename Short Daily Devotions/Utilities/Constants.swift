e31488aeb9a45f3f7e046e79bef5a8e2fcfd8389
MTTHWBSH <bushmat4@gmail.com>
2016-10-21 12:51:57 -0500

    remove instabug and add alamofire

---
+++ Short Daily Devotions/Utilities/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Short Daily Devotions
+//
+//  Created by Matt Bush on 10/21/16.
+//  Copyright © 2016 Matt Bush. All rights reserved.
+//
+
+let kBaseURL = "http://shortdailydevotions.com/wp-json/wp/v2/posts"
